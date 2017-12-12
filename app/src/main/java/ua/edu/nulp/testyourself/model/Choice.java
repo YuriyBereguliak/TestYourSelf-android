ee9eac653b0ebc8db0353af0b14576ed20e0b976
YuriyBereguliak <yuriy.bereguliak@inoxoft.com>
2017-12-12 15:37:58 +0200

    Added entites.

---
+++ app/src/main/java/ua/edu/nulp/testyourself/model/Choice.java
@@ -0,0 +1,73 @@
+package ua.edu.nulp.testyourself.model;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+/**
+ * TestYourSelf-android
+ * Created by Yuriy Bereguliak on 12/12/17.
+ */
+
+@Entity(tableName = "Choices")
+public class Choice {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "choice_id")
+    private int mChoiceId;
+
+    @ColumnInfo(name = "choice_text")
+    private String mChoiceText;
+
+    @ColumnInfo(name = "choice_is_true")
+    private boolean mIsChoiceTrue;
+
+    @ColumnInfo(name = "task_id")
+    private int mTaskId;
+
+    //region Getters and Setters
+    public int getChoiceId() {
+        return mChoiceId;
+    }
+
+    public void setChoiceId(int choiceId) {
+        mChoiceId = choiceId;
+    }
+
+    public String getChoiceText() {
+        return mChoiceText;
+    }
+
+    public void setChoiceText(String choiceText) {
+        mChoiceText = choiceText;
+    }
+
+    public boolean isChoiceTrue() {
+        return mIsChoiceTrue;
+    }
+
+    public void setChoiceTrue(boolean choiceTrue) {
+        mIsChoiceTrue = choiceTrue;
+    }
+
+    public int getTaskId() {
+        return mTaskId;
+    }
+
+    public void setTaskId(int taskId) {
+        mTaskId = taskId;
+    }
+    //endregion
+
+    //region Object
+    @Override
+    public String toString() {
+        return "Choice{" +
+                "mChoiceId=" + mChoiceId +
+                ", mChoiceText='" + mChoiceText + '\'' +
+                ", mIsChoiceTrue=" + mIsChoiceTrue +
+                ", mTaskId=" + mTaskId +
+                '}';
+    }
+    //endregion
+}
